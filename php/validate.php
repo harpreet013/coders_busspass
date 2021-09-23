c4ca1fb7c1290a6a008ed60bd2017d9d3ffd012f
harishvlog <harishkumar90151@gmail.com>
2021-09-23 14:24:14 +0530

    okk

---
+++ php/validate.php
@@ -0,0 +1,26 @@
+-
+        <b>F_Name is ->  </b><?php echo $fname; ?>
+        <br>
+        <b>L_Name is ->  </b><?php echo $lname; ?>
+        <br>
+        <b>Phone is ->  </b><?php echo $phone; ?>
+        <br>
+        <b>Email is ->  </b><?php echo $email; ?>
+        <br>
+        <b>Gender is ->  </b><?php echo $gender; ?>
+        <br>
+        <b>Category is ->  </b><?php echo $category; ?>
+        <br>
+        <b>Identity_id is ->  </b><?php echo $identity_id; ?>
+        <br>
+        <b>Identity_number is ->  </b><?php echo $identity_name; ?>
+        <br>
+        <b>Source is ->  </b><?php echo $source; ?>
+        <br>
+        <b>Destination is ->  </b><?php echo $destination; ?>
+        <br>
+        <b>From date is ->  </b><?php echo $fromdate; ?>
+        <br>
+        <b>To date is ->  </b><?php echo $todate; ?>
+        <br>
+        <b>Cost is ->  </b><?php echo $cost; ?> 
